c081ad2497a4a698f4e39c07f5b7f530c7cf4d81
aidenr <ranveer.aiden@genesisenergy.co.nz>
2021-07-24 11:33:41 +1200

    Hooks for test setup and cleanup

---
+++ AddBankAccNumber/AddBankAccNumber/Hooks/Web/WebHooks.cs
@@ -0,0 +1,36 @@
+﻿using AddBankAccNumber.Helpers;
+using AddBankAccNumber.POM.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AddBankAccNumber.Hooks.Web
+{
+    [Binding]
+    class WebHooks : BaseWebClass
+    {
+        [BeforeFeature]
+        public static void WebFeatureSetup()
+        {
+            envSetup = new EnvironmentSetup();
+
+            browser = envSetup.GetEnvironmentVariables("browser");
+            url = envSetup.GetEnvironmentVariables("browserUrl");
+            DefaultUsername = envSetup.GetEnvironmentVariables("defaultUsername");
+            DefaultPassword = envSetup.GetEnvironmentVariables("defaultPassword");
+            browserMgr = new BrowserManager();
+        }
+
+        [BeforeScenario]
+        public static void WebScenarioSetup()
+        {
+        }
+
+        [AfterScenario]
+        public void WebScenarioTeardown()
+        {
+            driver.Quit();
+        }
+    }
+}
